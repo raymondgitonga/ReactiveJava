f804e7b10aa545fac25e77f6c7d5556acaa0b8f3
raymond gitonga <raytosh95@gmail.com>
2020-05-06 01:29:57 +0300

    Implement amb()

--- src/main/java/MergeConcatObservables.java
+++ src/main/java/MergeConcatObservables.java
@@ -6,13 +6,16 @@ public class MergeConcatObservables {
 //        Observable<String> src1 = Observable.just("Ella", "Alexa","Lilly");
 //        Observable<String> src2 = Observable.just("Tosh", "Mellisa");
 
-        Observable<String> src1 = Observable.interval(1, TimeUnit.SECONDS).map(e -> "src1 : "+  e);
-        Observable<String> src2 = Observable.interval(1, TimeUnit.SECONDS).map(e -> "src2 : "+  e);
+        Observable<String> src1 = Observable.interval(1, TimeUnit.SECONDS).map(e -> "src1 : "+  e).take(10);
+        Observable<String> src2 = Observable.interval(1, TimeUnit.SECONDS).map(e -> "src2 : "+  e).take(10);
 
-        src1.mergeWith(src2)
+//        src1.mergeWith(src2)
+//                .subscribe(System.out::println);
+
+        src1.concatWith(src2)
                 .subscribe(System.out::println);
 
-        Thread.sleep(10000);
+        Thread.sleep(20000);
 
 //        Observable.concat(src1, src2)
 //                .subscribe(System.out::println);
