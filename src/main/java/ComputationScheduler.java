c9f72f369366722526ff6639ab42e7974e097876
raymond gitonga <raytosh95@gmail.com>
2020-05-10 20:57:38 +0300

    Implement schedulers

---
+++ src/main/java/ComputationScheduler.java
@@ -0,0 +1,26 @@
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class ComputationScheduler {
+    public static void main(String[] args) throws InterruptedException {
+        Observable<String> src = Observable.just("Pasta", "Pizza", "Fries", "Curry", "Chow mein")
+                .subscribeOn(Schedulers.computation());
+
+        src.subscribe(e -> compute());
+        src.subscribe(e -> compute());
+        src.subscribe(e -> compute());
+        src.subscribe(e -> compute());
+        src.subscribe(e -> compute());
+
+        Thread.sleep(5000);
+
+
+    }
+
+    public static void compute() throws InterruptedException{
+
+        Thread.sleep(1000);
+
+        System.out.println("Computation done by : " + Thread.currentThread().getName());
+    }
+}
