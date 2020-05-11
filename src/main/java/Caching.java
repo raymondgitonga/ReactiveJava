e2402091a443980c633e42c14e2bb5f609ee3fd1
raymond gitonga <raytosh95@gmail.com>
2020-05-11 23:13:08 +0300

    Implement Caching

---
+++ src/main/java/Caching.java
@@ -0,0 +1,17 @@
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+public class Caching {
+    public static void main(String[] args) throws InterruptedException {
+        Observable<Long> src = Observable.interval(1, TimeUnit.SECONDS)
+                .cache();
+        src.subscribe(e -> System.out.println("Observer 1  " + e));
+
+        Thread.sleep(5000);
+
+        src.subscribe(e -> System.out.println("Observer 2  " + e));
+
+        Thread.sleep(5000);
+    }
+}
